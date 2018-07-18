32d1e0dc3552fe694c66127e2b56d650dbdd9f2b
kristen <thehostboy@gmail.com>
2018-07-18 08:53:14 -0400

    lab 3 completed

---
+++ Lab3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string letsPlay;
+            string name;
+            do
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+
+                Console.WriteLine($"Hello {name}, nice to meet you! Want to play a game? (yes/no)");
+                letsPlay = Console.ReadLine();
+
+                while ((letsPlay == "y") || (letsPlay == "yes"))
+                {
+
+                    Console.WriteLine("Please choose a number between 1 and 100.");
+                    int answer = int.Parse(Console.ReadLine());
+
+                    if (answer <= 0)
+                    {
+                        Console.WriteLine("Please choose a positive number between 1 and 100.");
+                    }
+
+                    else if ((answer % 2 == 0) && (answer >= 2) && (answer <= 25))
+                    {
+                        Console.WriteLine($"Thanks, {name}! Number is even and less than 25.");
+                    }
+
+                    else if ((answer % 2 == 0) && (answer >= 26) && (answer <= 60))
+                    {
+                        Console.WriteLine($"Awesome, {name}! That's even!");
+                    }
+
+                    else if ((answer % 2 == 0) && (answer >= 60))
+                    {
+                        Console.WriteLine($"{answer} is even, thanks {name}!");
+                    }
+
+                    else if ((answer % 2 != 0) && (answer >= 60))
+                    {
+                        Console.WriteLine($"{answer} is an odd number, {name}.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{name}, you picked an odd number.");
+                    }
+
+                    Console.WriteLine($"Hey {name}, do you want to play again? y/n");
+                    letsPlay = Console.ReadLine();
+
+                }              
+
+            }
+            while ((letsPlay == "yes") || (letsPlay == "y"));
+            Console.WriteLine($"Ok, {name}, goodbye!");
+            
+
+        }
+        
+    }
+}
